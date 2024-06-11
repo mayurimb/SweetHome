87e2d71f4bda13a0a943eb7eb7904fc5d65c7366
bashirabadkar.m@northeastern.edu <bashirabadkar.m@northeastern.edu>
2024-06-11 15:33:51 -0400

    Committing changes in client before converting to submodule

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,143 @@
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken"
+ 
+export const getPosts = async (req, res) => {
+    const query = req.query;
+    console.log(query)
+    try{
+        const posts = await prisma.post.findMany({
+            where: {
+                city: query.city || undefined,
+                type: query.type || undefined,
+                property: query.property || undefined,
+                property: query.property || undefined,
+                bedroom: parseInt(query.bedroom) || undefined,
+                price: {
+                    gte: parseInt(query.minPrice) || 0,
+                    lte: parseInt(query.maxPrice) || 1000000,
+                }
+            }
+        })
+        //setTimeout(()=> {
+        res.status(200).json(posts), 3000
+        // })
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to get posts"})
+    }
+}
+ 
+export const getPost = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const post = await prisma.post.findUnique({
+            where: { id },
+            include: {
+                postDetail: true,
+                user: {
+                    select:{
+                        username: true,
+                        avatar: true
+                    }
+                },
+            }
+        });
+
+        let userId;
+        const token = req.cookies?.token;
+
+        // Function to verify token and return the user ID
+        const verifyToken = (token) => {
+            return new Promise((resolve, reject) => {
+                jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
+                    if (err) {
+                        reject(null); // If error, reject with null
+                    } else {
+                        resolve(payload.id); // Otherwise, resolve with payload ID
+                    }
+                });
+            });
+        };
+
+        if (token) {
+            try {
+                userId = await verifyToken(token);
+                //console.log("Verified User ID:", userId);
+            } catch (error) {
+                // console.log("JWT Verification Error:", error);
+                userId = null;
+            }
+        } else {
+            userId = null;
+        }
+
+        // Ensuring userId is not null before querying savedPost
+        const saved = userId ? await prisma.savedPost.findUnique({
+            where: {
+                userId_postId: {
+                    postId: id,
+                    userId
+                }
+            }
+        }) : null;
+
+        res.status(200).json({...post, isSaved: !!saved});
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({message: "Failed to get post"});
+    }
+};
+ 
+export const addPost = async (req, res) => {
+    const body = req.body
+    const tokenUserId = req.userId
+    try{
+        const newPost = await prisma.post.create({
+            data: {
+                ...body.postData,
+                userId: tokenUserId,
+                postDetail: {
+                    create: body.postDetail  
+                }
+            }
+        })
+        res.status(200).json(newPost)
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to create post"})
+    }
+}
+ 
+export const updatePost = async (req, res) => {
+    try{
+ 
+        res.status(200).json()
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to update post"})
+    }
+}
+ 
+export const deletePost = async (req, res) => {
+    const id  = req.params.id
+    const tokenUserId = req.userId
+ 
+    try{
+        const post = await prisma.post.findUnique(
+            {
+                where: {id}
+            }
+        )
+        if(post.userId !== tokenUserId)
+            return res.status(403).json({message: "Not authorized to delete the post!"})
+ 
+        await prisma.post.delete({
+            where: {id}
+        })
+        res.status(200).json({message: "Post deleted"})
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to delete post"})
+    }
+}
